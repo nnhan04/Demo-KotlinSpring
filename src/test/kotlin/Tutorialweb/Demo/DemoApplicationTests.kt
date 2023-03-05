cd6922bdbed26b44918a9a76ef143279fb0f416c
nnhan04 <nhoinhoi2401@gmail.com>
2023-03-05 22:56:47 +0700

    first commit

---
+++ src/test/kotlin/Tutorialweb/Demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package Tutorialweb.Demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
